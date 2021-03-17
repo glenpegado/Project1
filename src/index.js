a12c27493aa747720f1e449020e2d9af97c27fb3
Glen Pegado <glenpegado@Glens-MacBook-Air.local>
2021-03-17 13:51:09 -0400

    set up basic basic with event handler

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// make this into an array
+// loop through the array
+// add third book
+// add id
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const books = [
+  {
+    id: 1,
+    author: 'Jade Leaf Organic Matcha Green Tea Powder - Authentic Japanese Origin - Premium Second Harvest Culinary Grade (1.06 Ounce)',
+    image: 'https://m.media-amazon.com/images/I/51L9MGWV0yL._AC_SR500,460_.jpg',
+    title: 'Matcha title'
+  },
+  {
+    id: 2,
+    author: 'Visit the Sofia + Sam Store',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/91pb5oFC8JL._AC_SL1500_.jpg',
+    title: 'Sofia + Sam Oversized Memory Foam Lap Desk with Wrist Rest'
+  },
+  {
+    id: 3,
+    author: 'Visit the Samsonite Store',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/A1MocmxNcvL._AC_SL1500_.jpg',
+    title: 'Samsonite Omni PC Hardside Expandable Luggage with Spinner Wheels, Navy, Carry-On 20-Inch'
+  }
+]
+function BookList(){
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} {...book}/>
+      })}
+    </section>
+  )
+}
+
+const Book = ({image, title, author}) =>{
+  const clickHandler = () => {
+    alert('Hello')
+  }
+  return (
+      <article className='book'> 
+        <img src={image} alt=""/>
+        <h1>{title}</h1>
+        <h4>{author}</h4>
+        <button type='button' onClick={clickHandler}>
+          Glen
+        </button>
+      </article>
+  )
+}
+
+
+ReactDom.render(<BookList/>, document.getElementById('root'))
